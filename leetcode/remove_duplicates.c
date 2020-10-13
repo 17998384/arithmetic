8b0e732e373e9637dfbcff5e27cd4daae29e756e
葛振东 <gezhendong@fenxiangbuy.com>
2020-10-13 23:18:38 +0800

    新增算法

---
+++ leetcode/remove_duplicates.c
@@ -0,0 +1,34 @@
+//
+//  remove_duplicates.c
+//  arithmetic
+//
+//  Created by 我叫城北徐公° on 2020/10/13.
+//  Copyright © 2020 alex. All rights reserved.
+//
+
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize){
+    int count = (numsSize == 0) ? 0 : 1;
+    int point = 1;
+    for(int i = 0;i < numsSize;i++)
+    {
+        if(i > 0 && (nums[i] != nums[i - 1]))
+        {
+            nums[point++] = nums[i];
+            count++;
+        }
+    }
+    return count;
+}
+
+int main_remove_duplicates(void)
+{
+    int arr[] = {1,1,1,1,1,2,2,2,2,2,2};
+    int num = removeDuplicates(arr, sizeof(arr) / sizeof(int));
+    for (int i = 0; i < num; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
---
+++ leetcode/remove_element.c
@@ -0,0 +1,34 @@
+//
+//  remove_element.c
+//  arithmetic
+//
+//  Created by 我叫城北徐公° on 2020/10/13.
+//  Copyright © 2020 alex. All rights reserved.
+//
+
+#include <stdio.h>
+
+int removeElement(int* nums, int numsSize,int val){
+    int count = 0;
+    int point = 0;
+    for(int i = 0;i < numsSize;i++)
+    {
+        if((nums[i] != val))
+        {
+            nums[point++] = nums[i];
+            count++;
+        }
+    }
+    return count;
+}
+
+int main_remove_element(void)
+{
+    int arr[] = {3,2,2,3};
+    int num = removeElement(arr, sizeof(arr) / sizeof(int),3);
+    for (int i = 0; i < num; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
