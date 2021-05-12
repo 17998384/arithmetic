7a518f78124c3527df8aad10f53cddd84bdcf1a6
葛振东 <gezhendong@fenxiangbuy.com>
2021-05-12 11:13:21 +0800

    feat:新增算法

---
+++ leetcode/linked_list/insertion_sort_list.cpp
@@ -0,0 +1,69 @@
+//
+// Created by 我叫城北徐公° on 2021/5/12.
+//
+
+//  147. 对链表进行插入排序
+
+//  对链表进行插入排序。
+//
+//
+//  插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+//  每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+//
+//
+//
+//  插入排序算法：
+//
+//  插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+//  每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+//  重复直到所有输入数据插入完为止。
+//
+//
+//
+//  示例 1：
+//
+//  输入: 4->2->1->3
+//  输出: 1->2->3->4
+//
+//  示例 2：
+//
+//  输入: -1->5->3->4->0
+//  输出: -1->0->3->4->5
+
+
+#include <istream>
+#include <vector>
+#include "list_node.hpp"
+
+using namespace std;
+
+static bool my_compare(ListNode* a, ListNode* b)
+{
+    return a->val < b->val;
+}
+
+class Solution
+{
+public:
+
+    vector<ListNode*> v;
+
+    ListNode* insertionSortList(ListNode* head)
+    {
+        while (head)
+        {
+            v.push_back(head);
+            ListNode* next = head->next;
+            head->next = nullptr;
+            head = next;
+        }
+        sort(v.begin(), v.end(), my_compare);
+        int idx = 0;
+        while (idx < v.size() - 1)
+        {
+            v[idx]->next = v[idx + 1];
+            idx++;
+        }
+        return v[0];
+    }
+};
---
+++ leetcode/linked_list/reorder_list.cpp
@@ -0,0 +1,52 @@
+//
+// Created by 我叫城北徐公° on 2021/5/12.
+//
+
+//  143. 重排链表
+
+//  给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+//  将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+//
+//  你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+//
+//  示例 1:
+//
+//  给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+//
+//  示例 2:
+//
+//  给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+
+#include <iostream>
+#include <vector>
+#include "list_node.hpp"
+
+using namespace std;
+
+class Solution
+{
+public:
+
+    vector<ListNode*> v;
+
+    void reorderList(ListNode* head)
+    {
+        while (head)
+        {
+            v.push_back(head);
+            ListNode* next = head->next;
+            head->next = nullptr;
+            head = next;
+        }
+        int start = 0;
+        int end = v.size() - 1;
+        while (start < end)
+        {
+            v[start]->next = v[end];
+            if (start + 1 < end) v[end]->next = v[start + 1];
+            start++;
+            end--;
+        }
+
+    }
+};
